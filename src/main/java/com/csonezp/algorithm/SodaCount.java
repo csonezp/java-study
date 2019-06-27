c8696f1f84e709effd1fa8f7c1d94a777a940d30
csonezp <csonezp@gmail.com>
2019-06-27 20:30:19 +0800

    汽水瓶盖问题

--- src/main/java/com/csonezp/algorithm/SodaCount.java
+++ src/main/java/com/csonezp/algorithm/SodaCount.java
@@ -7,15 +7,22 @@ import java.util.Stack;
  */
 public class SodaCount {
 
+    /**
+     * n元钱最多能喝几瓶汽水
+     * @param n 金额数
+     * @return 喝了几瓶汽水
+     */
     public static int maxSodaCount(Integer n) {
+        //放汽水的栈
         Stack sodas = new Stack();
+        //放瓶盖的栈
         Stack caps = new Stack();
 
+        //喝汽水的总数
         Integer count = 0;
 
         for (int i = 0; i < n; i++) {
             sodas.push(1);
-            count++;
         }
 
         while (!sodas.empty() || caps.size() > 1) {
@@ -25,6 +32,8 @@ public class SodaCount {
             while (!sodas.empty()) {
                 //喝汽水
                 sodas.pop();
+                //喝了一瓶汽水，count+1
+                count++;
                 //放瓶盖
                 caps.push(1);
             }
@@ -34,7 +43,6 @@ public class SodaCount {
                 caps.pop();
                 caps.pop();
                 sodas.push(1);
-                count++;
             }
 
         }
